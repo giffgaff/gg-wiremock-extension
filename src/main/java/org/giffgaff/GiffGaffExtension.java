70233bed41ac04f42490828a711020a2dfd2d61f
Luke Vincent-Kynaston <luke.kynaston@giffgaff.co.uk>
2023-10-30 18:19:26 +0000

    DEVEX-547: update .gitignore

--- src/main/java/org/giffgaff/GiffGaffExtension.java
+++ src/main/java/org/giffgaff/GiffGaffExtension.java
@@ -1,34 +1,39 @@
 package org.giffgaff;
 
 
-import com.github.tomakehurst.wiremock.extension.Parameters;
-import com.github.tomakehurst.wiremock.extension.ServeEventListener;
-import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
-import com.github.tomakehurst.wiremock.verification.LoggedRequest;
+import static com.github.tomakehurst.wiremock.common.LocalNotifier.notifier;
+
+import com.github.tomakehurst.wiremock.extension.requestfilter.RequestFilterAction;
+import com.github.tomakehurst.wiremock.extension.requestfilter.StubRequestFilter;
+import com.github.tomakehurst.wiremock.http.Request;
+import java.io.File;
 import org.slf4j.MDC;
 
-public class GiffGaffExtension implements ServeEventListener {
+public class GiffGaffExtension extends StubRequestFilter {
 
-    private static final String TRACE_JSON_VALUE = "traceId";
-    private static final String SPAN_JSON_VALUE = "spanId";
-    private static final String TRACE_ID_HEADER = "X-B3-TraceId";
-    private static final String SPAN_ID_HEADER = "X-B3-SpanId";
+  private static final String TRACE_JSON_VALUE = "traceId";
+  private static final String SPAN_JSON_VALUE = "spanId";
+  private static final String TRACE_ID_HEADER = "X-B3-TraceId";
+  private static final String SPAN_ID_HEADER = "X-B3-SpanId";
 
-    @Override
-    public String getName() {
-        return "giffgaff-extension";
+  @Override
+  public String getName() {
+    return "giffgaff-extension";
+  }
+
+
+  @Override
+  public RequestFilterAction filter(Request request) {
+    MDC.put(TRACE_JSON_VALUE, request.getHeader(TRACE_ID_HEADER));
+    MDC.put(SPAN_JSON_VALUE, "test2");
+    notifier().error("Request trace id: " + request.getHeader(TRACE_ID_HEADER));
+    notifier().error("Request span id: " + request.getHeader(SPAN_ID_HEADER));
+
+    String classpath = System.getProperty("java.class.path");
+    String[] classpathEntries = classpath.split(File.pathSeparator);
+    for (String entry : classpathEntries) {
+      notifier().error("Classpath entry: " + entry);
     }
-
-    @Override
-    public void beforeMatch(ServeEvent serveEvent, Parameters parameters) {
-        LoggedRequest request = serveEvent.getRequest();
-
-        MDC.put(TRACE_JSON_VALUE, request.getHeader(TRACE_ID_HEADER));
-        MDC.put(SPAN_JSON_VALUE, request.getHeader(SPAN_ID_HEADER));
-    }
-
-    @Override
-    public void afterComplete(ServeEvent serveEvent, Parameters parameters) {
-        MDC.clear();
-    }
+    return RequestFilterAction.continueWith(request);
+  }
 }
